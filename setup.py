5a4304c00dee26fc0fe2fc17d144974623a9397c
Adam Green <adam.green@adgefficiency.com>
2020-03-08 12:58:12 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Solution to the Data Science Retreat NLP for stock price prediction minicomp.',
+    author='Adam Green - adgefficiency.com',
+    license='MIT',
+)
